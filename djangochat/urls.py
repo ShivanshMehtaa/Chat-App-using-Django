f17b7b4a4db47204d640b1a979efd53722ce73c1
Shivansh mehta <112550037+ShivanshMehtaa@users.noreply.github.com>
2022-10-29 23:20:56 +0530

    Add files via upload

---
+++ djangochat/urls.py
@@ -0,0 +1,10 @@
+
+import imp
+from django.contrib import admin
+from django.urls import path, include
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('chat.urls'))
+]
